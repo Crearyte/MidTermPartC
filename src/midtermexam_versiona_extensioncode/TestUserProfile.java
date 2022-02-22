b54fd73d7e7b9d8af3abc2803d50973ef8db791f
TerrelC <crearyte@sheridanc.on.ca>
2022-02-22 13:16:03 -0500

    Created TestUserProfile The user will enter their name and choose their favourite Genre from a displayed list of the possible Genres. The user will then see a message that their userProfile was created.

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,10 @@
+package midtermexam_versiona_extensioncode;
+import java.util.*;
+public class TestUserProfile{
+/*The user will enter their name and choose their favourite Genre from a 
+displayed list of the possible Genres. The user will then see a message that 
+their userProfile was created.*/ 
+    public static void main(String[] args){
+    
+    }
+}
